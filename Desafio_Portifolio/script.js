0c5de5090b22e5574fb51a28c64339d9b30fafb6
Eliel Cesar da Silva Lima <eliel.tecinfo@gmail.com>
2023-06-03 18:01:23 -0400

    Commit
    update files

---
+++ Desafio_Portifolio/script.js
@@ -0,0 +1,8 @@
+// vars
+const btn_enviar = document.querySelector('#enviar_email')
+
+btn_enviar.addEventListener('click', () => {
+
+    alert('No momento não estamos recebendo e-mails pela página!')
+
+})
